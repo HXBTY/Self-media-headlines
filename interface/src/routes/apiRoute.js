bfa2eaa28250fb591d13d19aea165d334cdc8320
LYH <lin2770529951@163.com>
2020-05-24 13:33:01 +0800

    完成login, 开始编写home

---
+++ interface/src/routes/apiRoute.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+
+let route = express.Router()
+const apiCtrl = require('../controllers/apiController.js')
+
+// 头像修改
+let url_user = path.join(__dirname,'../public/user_photo')
+const storage_user = multer.diskStorage({
+  destination(req,file,cb) {
+    cb(null,url_user)
+  },
+  filename(req,file,cb) {
+    cb(null,Date.now() + file.originalname)
+  }
+})
+const upload_user = multer({storage: storage_user}) 
+
+// 创建新闻上传图片
+let url_news = path.join(__dirname,'../public/news_img')
+const storage_news = multer.diskStorage({
+  destination(req,file,cb) {
+    cb(null,url_news)
+  },
+  filename(req,file,cb) {
+    cb(null,Date.now() + file.originalname)
+  }
+})
+const upload_news = multer({storage: storage_news}) 
+
+// 登录验证
+route.post('/api/login',apiCtrl.login);
+
+// 新闻类型
+route.get('/api/newstype',apiCtrl.newstype);
+
+// 新闻列表
+route.get('/api/newslist',apiCtrl.newslist);
+
+// 新增新闻
+route.post('/api/newsadd',apiCtrl.newsadd);
+
+// 新增新闻图片地址获取
+route.post('/api/newsadd_imgname',upload_news.single('file'),apiCtrl.newsadd_imgname)
+
+// 删除新闻
+route.get('/api/newsdel',apiCtrl.newsdel);
+
+// 获取新闻根据id
+route.get('/api/newsbyid',apiCtrl.newsbyid);
+
+// 修改新闻
+route.post('/api/newsedit',apiCtrl.newsedit);
+
+// 获取个人信息
+route.get('/api/account',apiCtrl.account);
+
+// 修改后台登录密码
+route.post('/api/pwdedit',apiCtrl.pwdedit);
+
+// 审核改变状态
+route.post('/api/editstatus',apiCtrl.editstatus);
+
+// 修改头像
+route.post('/api/userphoto',upload_user.single('file'),apiCtrl.userphoto)
+
+// 获取新闻类型数量
+route.get('/api/pie_type',apiCtrl.pie_type)
+
+// 获取新闻状态数量
+route.get('/api/pie_status',apiCtrl.pie_status)
+module.exports = route
--- my-work/src/views/home/index.vue
+++ my-work/src/views/home/index.vue
@@ -1,6 +1,47 @@
 <template>
   <div id="home">
-    登录主页
+    <div class="left">
+      <a href="###">
+        <img src="./images/logo-home.gif" alt="" style="width: 110px;" />
+      </a>
+      <ul>
+        <li>
+          <a href="###" class="active">推荐</a>
+        </li>
+        <li>
+          <a href="###">热点</a>
+        </li>
+        <li>
+          <a href="###">直播</a>
+        </li>
+        <li>
+          <a href="###">图片</a>
+        </li>
+        <li>
+          <a href="###">科技</a>
+        </li>
+        <li>
+          <a href="###">娱乐</a>
+        </li>
+        <li>
+          <a href="###">体育</a>
+        </li>
+        <li>
+          <a href="###">懂车帝</a>
+        </li>
+        <li>
+          <a href="###">财经</a>
+        </li>
+        <li>
+          <a href="###">搞笑</a>
+        </li>
+        <li>
+          <a href="###">更多</a>
+        </li>
+      </ul>
+    </div>
+    <div class="content"></div>
+    <div class="right"></div>
   </div>
 </template>
 
@@ -9,4 +50,32 @@ export default {}
 </script>
 
 <style lang="less">
+#home {
+  padding-top: 15px;
+  height: 100%;
+  width: 1170px;
+  margin: 0 auto;
+}
+.left {
+  height: 100%;
+  background: chartreuse;
+}
+.content {
+  background: green;
+  height: 100%;
+}
+.right {
+  background: blue;
+  height: 100%;
+}
+a {
+  display: block;
+  width: 110px;
+  line-height: 40px;
+  text-align: center;
+}
+.active {
+  background-color: #ed4040;
+  color: #fff;
+}
 </style>
